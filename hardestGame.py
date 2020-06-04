e45678d17e7b4d2df8e6098648631e97aa0d282a
Leticia Berto <leticia_berto@hotmail.com>
2020-06-04 10:15:10 -0300

    Add boundaries detection

--- hardestGame.py
+++ hardestGame.py
@@ -4,12 +4,16 @@ import random
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400))
+height = 400
+width = 600
+
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Hardest Game')
 
 # Background
 background = pygame.image.load('background.png')
 
+#Goal
 startPlaceX = [100]
 endPlaceX = [480]
 endPlaceY = [0,20]
@@ -18,11 +22,23 @@ clock = pygame.time.Clock()
 
 tamBlocks = 20 #qntd de pixels que os objetos ocupam -- no caso matrix cada elemento da matrix tem 10px
 
+game_over = False
+
 #player
 player = [300, 300]
 player_skin = pygame.Surface((tamBlocks,tamBlocks))
 player_skin.fill((255,255,255)) #White
 
+# Macro definition for player movement.
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+STOP = 4
+
+my_direction = STOP
+
+# Enemy
 x_enemyIni = 400
 y_enemyIni = 60
 
@@ -32,9 +48,6 @@ distYBetweenEnemies = 100
 blue_skin = pygame.Surface((tamBlocks,tamBlocks))
 blue_skin.fill((0,0,255)) #White
 
-game_over = False
-
-# Enemy
 enemyImg = []
 enemyX = []
 enemyY = []
@@ -61,29 +74,30 @@ def check_end(player):
     return (player[0] > endPlaceX[0]) and (endPlaceY[0] <= player[1] <= endPlaceY[1])
 
 while not game_over:
+
     clock.tick(10)
     screen.fill((0,0,0))
     screen.blit(background, (0, 0))
-    #screen.blit(startPlace_skin,startPlace)
-    #screen.blit(endPlace_skin, endPlace)
-
+    my_direction = STOP
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
 
-    if event.type == KEYDOWN:
-        if event.key == K_UP:
-            player = (player[0], player[1] - tamBlocks)
-        if event.key == K_DOWN:
-            player = (player[0], player[1] + tamBlocks)
-        if event.key == K_LEFT:
-            player = (player[0] - tamBlocks, player[1])
-        if event.key == K_RIGHT:
-            player = (player[0] + tamBlocks, player[1])
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_direction = UP
+            if event.key == K_DOWN:
+                my_direction = DOWN
+            if event.key == K_LEFT:
+                my_direction = LEFT
+            if event.key == K_RIGHT:
+                my_direction = RIGHT
+            #else:
+                #my_direction = STOP
 
-    #limite maximo para os inimigos se movimentarem
+
     # Enemy Movement
     for i in range(num_of_enemies):
         enemyX[i] += enemyX_change[i]
@@ -96,14 +110,33 @@ while not game_over:
 
         enemy(enemyX[i], enemyY[i], i)
 
+    # Check if player collided with enemies
     for i in range(num_of_enemies):
         if(collision(player,i)):
             game_over = True
             break
 
+    playerChangeY = 0
+    playerChangeX = 0
+
+        # Actually make the Player move.
+    if my_direction == UP and player[1] > 0:
+        playerChangeY = -tamBlocks
+    if my_direction == DOWN  and player[1] < height - tamBlocks:
+        playerChangeY = tamBlocks
+    if my_direction == RIGHT and player[0] < width - tamBlocks:
+        playerChangeX = tamBlocks
+    if my_direction == LEFT and player[0] > 0:
+        playerChangeX = -tamBlocks
+    print(player)
+
+    player = (player[0] + playerChangeX, player[1] + playerChangeY)
+
+    #Player movement
     screen.blit(player_skin,player)
 
+    #check if is the end point
     if (check_end(player)):
         game_over = True
-    print(player[0], " ",  endPlaceX[0], " ", endPlaceY[0], " ", player[1], " ", endPlaceY[1])
+
     pygame.display.update()
