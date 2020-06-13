ce19065fa7f805bc25dd10182ea2470c252607c8
Leticia Berto <leticia_berto@hotmail.com>
2020-06-13 19:04:30 -0300

    added mutation with swap in two points alleatory

--- genetic_algorithm/ga_exchange_entire_population.py
+++ genetic_algorithm/ga_exchange_entire_population.py
@@ -3,26 +3,26 @@ from src.Actions import Actions
 from .ga_lib import weighted_shuffle
 
 class Individual:
-    
+
     def __init__(self, state):
         self.state = state
         self.best_position = 1000
         self.action_best_position = -1
         self.poison = 0
         self.win = False
-        
-    def fitness(self): 
+
+    def fitness(self):
         return self.poison + (1*self.best_position) + self.action_best_position #menor dist para o goal com o menor numero de acoes
 #
-def crossover(s1, s2, point): 
+def crossover(s1, s2, point):
     return s1[:point] + s2[point:], s2[:point] + s1[point:]
 
-def selection(population): 
+def selection(population):
     all_fitness = [individual.fitness() for individual in population]
-    
+
     total = sum(all_fitness)
     p = [(1-(fitness/total)) for fitness in all_fitness]
-    
+
     new = weighted_shuffle(population, p)
 
     return new
@@ -35,10 +35,13 @@ def increase_state(population, n_state, d):
         individual.state = state[:n_state]
 
     return population
-    
-def mutation(state, point, d):
-    d = random.randint(1, d) 
-    state[point:point+d] = [random.choice(list(Actions)).value]*d
+
+def mutation(state):
+    point_one = random.randint(0, len(state)-1)
+    point_two = random.randint(0, len(state)-1)
+    #print(point_one)
+    #print(point_two)
+    state[point_one], state[point_two] = state[point_two], state[point_one]
     return state
 
 def create_initial_population(size, n_state, d):
--- simulation_exchange_entire_population.py
+++ simulation_exchange_entire_population.py
@@ -3,66 +3,66 @@ import genetic_algorithm.ga_exchange_entire_population as ga
 import random
 
 def simulate_game():
-	population_size = 100
-	max_iterations = 2000
-	best = None
-	worst = None
-	state_size = 15 #aumenta o tamanho do cromossomo gradativamente, cinco estados a cada cinco gerações
-	i = 0
-	j = 0
+    population_size = 100
+    max_iterations = 2000
+    best = None
+    worst = None
+    state_size = 15 #aumenta o tamanho do cromossomo gradativamente, cinco estados a cada cinco gerações
+    i = 0
+    j = 0
+    mutation_rate = 0.03
 
-	d = int(state_size/2)
+    d = int(state_size/2)
 
-	population = ga.create_initial_population(population_size, state_size, d)
+    population = ga.create_initial_population(population_size, state_size, d)
 
-	app = App(len(population), display=True)
+    app = App(len(population), display=True)
 
-	while(j < max_iterations):
+    while(j < max_iterations):
 
-		d = int(state_size/2) #max number of repetitions
+        d = int(state_size/2) #max number of repetitions
 
-		print("Starting simulation of generation " + str(j), state_size)
-	
-		results = app.run([individual.state[:state_size] for individual in population], (state_size))
+        print("Starting simulation of generation " + str(j), state_size)
 
-		for i in range(len(population)):
-			population[i].win = results[i][0]
-			population[i].best_position = results[i][1]
-			population[i].action_best_position = results[i][2]
-			population[i].poison = results[i][3]
+        results = app.run([individual.state[:state_size] for individual in population], (state_size))
 
-			if population[i].win:
-				print("Alguém venceu!!!")
+        for i in range(len(population)):
+            population[i].win = results[i][0]
+            population[i].best_position = results[i][1]
+            population[i].action_best_position = results[i][2]
+            population[i].poison = results[i][3]
 
-			if not best or population[i].fitness() < best.fitness():
-				best = population[i]
+            if population[i].win:
+                print("Alguém venceu!!!")
 
-			if not worst or population[i].fitness() > worst.fitness():
-				worst = population[i]
+            if not best or population[i].fitness() < best.fitness():
+                best = population[i]
 
-		new_population = []
-		
-		parents_selection = ga.selection(population)
-		for i in range(int(len(parents_selection)/2)):
-			new_ind_1_crom, new_ind_2_crom = ga.crossover(parents_selection[i].state, parents_selection[i+1].state, state_size - 10)
-			new_population.append(ga.Individual(new_ind_1_crom))
-			new_population.append(ga.Individual(new_ind_2_crom))
-			#new_population.append(ga.Individual(ga.mutation(ind1.state, ind1.action_best_position-2, d))) #tenta fazer mutação bem perto da melhor posição
-			#new_population.append(ga.Individual(ga.mutation(ind2.state, ind2.action_best_position-2, d)))
-		
-		population =  new_population
-		
-		j += 1
+            if not worst or population[i].fitness() > worst.fitness():
+                worst = population[i]
 
-		if j%5 == 0:
-			state_size += 20
-			population = ga.increase_state(population, state_size, d)
+        new_population = []
 
-	print(state_size)
+        parents_selection = ga.selection(population)
+        for i in range(int(len(parents_selection)/2)):
+            new_ind_1_crom, new_ind_2_crom = ga.crossover(parents_selection[i].state, parents_selection[i+1].state, state_size - 10)
+
+            if random.random() <= mutation_rate:
+                new_ind_1_crom = ga.mutation(new_ind_1_crom)
+                new_ind_2_crom = ga.mutation(new_ind_2_crom)
+
+            new_population.append(ga.Individual(new_ind_1_crom))
+            new_population.append(ga.Individual(new_ind_2_crom))
+
+        population =  new_population
+
+        j += 1
+
+        if j%5 == 0 and state_size <=500:
+            state_size += 5
+            population = ga.increase_state(population, state_size, d)
+
+    print(state_size)
 
 if __name__ == "__main__":
-	simulate_game()
-
-
-
-	
+    simulate_game()
