7ab5f5db3b7d661623ea25e9b6c91fe62150c474
Gabriel Previato <gabriel.previato@gmail.com>
2020-06-15 13:16:51 -0300

    Added timestamp to graphs files

--- simulation.py
+++ simulation.py
@@ -3,23 +3,24 @@ import genetic_algorithm.ga as ga
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 
-def create_mean_graphic(pop_min, pop_max, pop_mean, ax_mean, fig_mean, fig_mean_file, index):
+def create_mean_graphic(time, pop_min, pop_max, pop_mean, ax_mean, fig_mean, fig_mean_file, index):
 	pop_error = [pop_max, pop_min]
 	error_interval = (index // 50) + 1
 	ax_mean.errorbar(np.arange(index + 1), pop_mean, yerr=pop_error, errorevery=error_interval)
 
 	fig_mean.suptitle('Mean Fitness')
-	fig_mean.savefig(fig_mean_file, dpi=100)
+	fig_mean.savefig(str(time) + fig_mean_file, dpi=100)
 	plt.close(fig_mean)
 
 
-def create_best_graphic(pop_best, ax_best, fig_best, fig_best_file, index):
+def create_best_graphic(time, pop_best, ax_best, fig_best, fig_best_file, index):
 	ax_best.plot(np.arange(index + 1), pop_best)
 
 	fig_best.suptitle('Best Fitness')
-	fig_best.savefig(fig_best_file, dpi=100)
+	fig_best.savefig(str(time) + fig_best_file, dpi=100)
 	plt.close(fig_best)
 
 
@@ -31,6 +32,10 @@ def simulate_game():
 	j = 0
 
 	d = int(state_size/2)
+	
+	now = datetime.now()
+	# dd/mm/YY-H:M:S
+	dt_string = now.strftime("%d/%m/%Y-%H:%M:%S")
 
 	population = ga.create_initial_population(population_size, state_size, d)
 
@@ -80,11 +85,11 @@ def simulate_game():
 
 		pop_best.append(best.fitness())
 
-		create_mean_graphic(pop_min_error, pop_max_error, pop_mean, ax_mean, fig_mean, fig_mean_file, j)
-		app.update_mean_graph(fig_mean_file)
+		create_mean_graphic(dt_string, pop_min_error, pop_max_error, pop_mean, ax_mean, fig_mean, fig_mean_file, j)
+		app.update_mean_graph(str(dt_string) + fig_mean_file)
 
-		create_best_graphic(pop_best, ax_best, fig_best, fig_best_file, j)
-		app.update_max_graph(fig_best_file)
+		create_best_graphic(dt_string, pop_best, ax_best, fig_best, fig_best_file, j)
+		app.update_max_graph(str(dt_string) + fig_best_file)
 
 		new_population = []
 		
