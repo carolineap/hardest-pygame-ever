385b4fa0f6620035ae4e0d5c8aeceef856e79fbb
carolineap <hicaarol@gmail.com>
2020-06-15 10:16:53 -0300

    change distance and poison methods, write results in file, fix enemies bug

--- src/AppSimulation.py
+++ src/AppSimulation.py
@@ -1,6 +1,8 @@
+ 
 import pygame
 import math
 import time
+import numpy
 
 from src.LevelOne import LevelOne
 from src.ControllerSimulation import ControllerSimulation as Controller
@@ -10,124 +12,128 @@ from src.Enemy import Enemy
 from src.Score import Score
 
 class AppSimulation:
-    def __init__(self, population_size, display=True):
-        pygame.init()
-        self.controller = Controller()
+	def __init__(self, population_size, display=True):
+		pygame.init()
+		self.controller = Controller()
 
-        self.players = []
-        for i in range(population_size):
-            self.players.append((Player(pygame.Vector2(0, 0))))
+		self.players = []
+		for i in range(population_size):
+			self.players.append((Player(pygame.Vector2(0, 0))))
 
-        self.clock = pygame.time.Clock()
-        self.running = False
+		if population_size > 50:
+			rng = numpy.random.default_rng(42)
+			self.drawable_players = rng.choice(self.players, 50, replace=False)
+		else:
+			self.drawable_players = self.players
 
-        self.action = ""
-        self.value = 0
+		self.clock = pygame.time.Clock()
+		self.running = False
 
-        self.background_color = (200, 200, 200)
-        self.level_one = LevelOne(30, self.background_color)
+		self.action = ""
+		self.value = 0
 
-        self.restart()
+		self.background_color = (200, 200, 200)
+		self.level_one = LevelOne(30, self.background_color)
 
-        self.display = display
+		self.restart()
 
-        if display:
-            self.main_screen = Screen(background_color=self.background_color)
-            pygame.mixer.music.load("SoundTrack/hardestGameThemeSong.mp3")
-            pygame.mixer.music.play(-1)
-            pygame.mixer.music.set_volume(0.5)
+		self.display = display
 
-            self.mean_graph_surface = pygame.Surface((300, 300))
+		if display:
+			self.main_screen = Screen(background_color=self.background_color)
+			# pygame.mixer.music.load("SoundTrack/hardestGameThemeSong.mp3")
+			# pygame.mixer.music.play(-1)
+			# pygame.mixer.music.set_volume(0.5)
 
-    def run(self, actions, n):        
-        self.restart()
-        
-        results = []
-        for i in range(len(self.players)):
-            results.append([False, 1000, -1, 0])
+			self.mean_graph_surface = pygame.Surface((300, 300))
 
-        for i in range(n):
-            
-            ms_dt = self.clock.tick(60)    
-            s_dt = ms_dt / 1000
+	def run(self, actions, n):        
+		self.restart()
+		
+		results = []
+		for i in range(len(self.players)):
+			results.append([False, 1000, -1, 0])
 
-            self.move_enemies(s_dt)
-            
-            not_dead = False
+		for i in range(n):
+			
+			ms_dt = self.clock.tick(60)    
+			s_dt = ms_dt / 1000
 
-            for j in range(len(self.players)):
-                if not self.players[j].dead:
-                    not_dead = True
-                    self.input_loop(actions[j][i])
+			self.move_enemies(s_dt)
+			
+			not_dead = False
 
-                    results[j][0] = self.exec_loop(s_dt, j) #win
-                    results[j][3] = max(results[j][3] + self.level_one.poison_player(self.players[j]), 0)
+			for j in range(len(self.players)):
 
-                    if results[j][1] > self.level_one.distance(self.players[j]):
-                        results[j][1] = self.level_one.distance(self.players[j]) #get value and action for best position (closest to the goal)
-                        results[j][2] = i
+				if not self.players[j].dead:
+					not_dead = True
+					self.input_loop(actions[j][i])
 
-            if not not_dead:
-                break
-            if self.display:
-                self.render_loop()  
+					results[j][0] = self.exec_loop(s_dt, j) #win
+					results[j][3] = self.level_one.poison_player(self.players[j])
 
+					if results[j][1] > self.level_one.distance(self.players[j]):
+						results[j][1] = self.level_one.distance(self.players[j]) #get value and action for best position (closest to the goal)
+						results[j][2] = i
 
-        return results
+			if not not_dead:
+				break
+			if self.display:
+				self.render_loop()  
 
-
-    def input_loop(self, action):
-        self.action, self.value = self.controller.read_input(action)
-
-    def move_enemies(self, dt):
-        for enemy in self.enemies:
-            enemy.move(dt)
-
-    def exec_loop(self, dt, j):
-        if self.action == 'exit':
-            pygame.quit()
-            exit(0)
-
-        self.players[j].move(self.value, dt)
-
-        if self.players[j].is_dead(self.enemies):
-            return False
-            
-        # Check if won
-        if self.level_one.end_area.colliderect(self.players[j].collision_box()):
-            return True
-
-        return False
-                
-    def restart(self):
-        for player in self.players:
-            player.set_init_pos(self.level_one.player_init)
-            player.set_current_level(self.level_one)
-            player.set_dead(False) 
-
-        enemy_mov_period = 1.5
-        self.enemies = [
-            Enemy(self.level_one.enemies_init[i], self.level_one.enemies_init_direct[i], 'h', enemy_mov_period,
-                  self.level_one.enemies_trajectory[i][0], self.level_one.enemies_trajectory[i][1])
-            for i in range(len(self.level_one.enemies_init))
-        ]
-
-    def render_loop(self):
-        self.level_one.draw_level_in_surface()
-
-        for player in self.players:
-            if not player.dead:
-                player.draw_player(self.level_one.surface)
-            
-        for enemy in self.enemies:
-            enemy.draw_enemy(self.level_one.surface)
-
-        self.main_screen.update_screen(self.level_one, self.mean_graph_surface)
-        
-        pygame.display.update()
-
-    def update_mean_graph(self, fig_mean_file):
-        mean_graph = pygame.image.load(fig_mean_file)
-        self.mean_graph_surface.blit(mean_graph, (0, 0))
+		return results
 
 
+	def input_loop(self, action):
+		self.action, self.value = self.controller.read_input(action)
+
+	def move_enemies(self, dt):
+		for enemy in self.enemies:
+			enemy.move(dt)
+
+	def exec_loop(self, dt, j):
+		if self.action == 'exit':
+			pygame.quit()
+			exit(0)
+
+		self.players[j].move(self.value, dt)
+
+		if self.players[j].is_dead(self.enemies):
+			return False
+			
+		# Check if won
+		if self.level_one.end_area.colliderect(self.players[j].collision_box()):
+			return True
+
+		return False
+				
+	def restart(self):
+		for player in self.players:
+			player.set_init_pos(self.level_one.player_init)
+			player.set_current_level(self.level_one)
+			player.set_dead(False) 
+
+		enemy_mov_period = 1.5
+		self.enemies = [
+			Enemy(self.level_one.enemies_init[i], self.level_one.enemies_init_direct[i], 'h', enemy_mov_period,
+				  self.level_one.enemies_trajectory[i][0], self.level_one.enemies_trajectory[i][1])
+			for i in range(len(self.level_one.enemies_init))
+		]
+
+	def render_loop(self):
+		self.level_one.draw_level_in_surface()
+
+		for player in self.drawable_players:
+			if not player.dead:
+				player.draw_player(self.level_one.surface)
+			
+		for enemy in self.enemies:
+			enemy.draw_enemy(self.level_one.surface)
+
+		self.main_screen.update_screen(self.level_one, self.mean_graph_surface)
+		
+		pygame.display.update()
+
+	def update_mean_graph(self, fig_mean_file):
+		mean_graph = pygame.image.load(fig_mean_file)
+		self.mean_graph_surface.blit(mean_graph, (0, 0))
--- src/Enemy.py
+++ src/Enemy.py
@@ -3,40 +3,43 @@ import pygame
 
 
 class Enemy:
-    def __init__(self, initial_pos : pygame.Vector2, initial_direction, trajectory_type, trajectory_period, trajectory_begin, trajectory_end):
-        self.position = initial_pos
-        self.direction = initial_direction
+	def __init__(self, initial_pos : pygame.Vector2, initial_direction, trajectory_type, trajectory_period, trajectory_begin, trajectory_end):
+		self.position = initial_pos
+		self.direction = initial_direction
 
-        self.trajectory_type = trajectory_type
-        self.trajectory_begin = trajectory_begin
-        self.trajectory_end = trajectory_end
-        self.trajectory_period = trajectory_period
+		self.trajectory_type = trajectory_type
+		self.trajectory_begin = trajectory_begin
+		self.trajectory_end = trajectory_end
+		self.trajectory_period = trajectory_period
 
-        if trajectory_type == 'h':
-            self.velocity = pygame.Vector2((trajectory_end - trajectory_begin) / trajectory_period, 0)
+		if trajectory_type == 'h':
+			self.velocity = pygame.Vector2((trajectory_end - trajectory_begin) / trajectory_period, 0)
 
-        self.radius = 9
-        self.position = self.position.elementwise() - self.radius
+		self.radius = 9
+		self.position = self.position.elementwise() - self.radius
 
-        self.color = (0, 0, 255)
-        self.current_level = None
+		self.color = (0, 0, 255)
+		self.current_level = None
+		self.moves_after_inversion = 0
 
-    def move(self, dt: float):
-        if self.is_at_end():
-            self.direction *= -1
+	def move(self, dt: float):
+		if self.is_at_end():
+			self.direction *= -1
+			self.moves_after_inversion = 0
 
-        self.position += self.velocity * dt * self.direction
+		self.position += self.velocity * dt * self.direction
+		self.moves_after_inversion += 1
 
-    def is_at_end(self):
-        if self.position.x >= self.trajectory_end - 3 * self.radius or \
-            self.position.x <= self.trajectory_begin + self.radius:
-            return True
+	def is_at_end(self):
+		if ((self.position.x >= self.trajectory_end - 3 * self.radius or
+			self.position.x <= self.trajectory_begin + self.radius) and
+			 self.moves_after_inversion > 5):
+			return True
 
-        return False
+		return False
 
-    def collision_box(self):
-        return pygame.Rect((int(self.position.x), int(self.position.y)), (self.radius * 2, self.radius * 2))
+	def collision_box(self):
+		return pygame.Rect((int(self.position.x), int(self.position.y)), (self.radius * 2, self.radius * 2))
 
-    def draw_enemy(self, surface):
-        ret = pygame.draw.circle(surface, self.color, (int(self.position.x + self.radius), int(self.position.y + self.radius)), self.radius)
-        #print(ret)
+	def draw_enemy(self, surface):
+		ret = pygame.draw.circle(surface, self.color, (int(self.position.x + self.radius), int(self.position.y + self.radius)), self.radius)
